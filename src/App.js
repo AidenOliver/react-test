6478b30f39a70731e25b63a8feff2c118acf4eb4
Aiden Oliver <31047390+AidenOliver@users.noreply.github.com>
2020-04-16 17:27:15 -0600

    Added input to to the screen

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
 import React, {useState, useEffect, useRef} from 'react';
 import './App.css';
 
-function App() {
-  const [value, setValue] = useState('');
-  const nameInput = useRef();
-  
-  function handleFormSubmit(event) {
-    event.preventDefault();
-    const body = {
-      name: nameInput.current.value
-    }
+function Item({item, index}) {
+  return (
+    <div className="item">
+      {item.text}
+    </div>
+  )
+}
+
+function ItemForm({addItem}) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    addItem(value);
+    setValue("");
   }
   
   return (
     <div className="App">
-      <div className="getName">
-        <form onSubmit={handleFormSubmit}>
-          <input ref={nameInput} type="text" />
-          <input type="submit" onSubmit={handleFormSubmit}/>
+        <form onSubmit={handleSubmit}>
+          <input 
+            type="text" 
+            value={value}
+            onChange={e => setValue(e.target.value)}
+          />
+          
         </form>
+    </div>
+  );
+}
+
+function App() {
+  const [items, setItems] = useState([]);
+
+  const addItem = text => {
+    const newItems = [...items, {text}];
+    setItems(newItems);
+  }
+
+  return (
+    <div className="App">
+      <div className="item-list">
+        {items.map((item, index) => (
+          <Item 
+            key={index}
+            index={index}
+            item={item}
+          />
+        ))}
+        <ItemForm addItem={addItem} />
       </div>
     </div>
   );
